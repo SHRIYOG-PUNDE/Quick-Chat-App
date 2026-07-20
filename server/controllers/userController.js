4ea405bc46b463eb1d262ccdaed464940f9d065b
Shriyog <pundeshriyog2004@gmail.com>
2026-07-20 22:28:12 +0530

    url change

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -65,19 +65,19 @@ export const checkAuth = (req, res)=>{
 
 // Controller to update user profile details
 
-export const updateProfile = async(req, res)=>{
+export const updateProfile = async(req, res) => {
     try {
         const {profilePic, bio, fullName} = req.body;
-
         const userId = req.user._id;
         let updatedUser;
 
         if(!profilePic){
-            await User.findByIdAndUpdate(userId, {fullName, bio}, {new:true})
-        }else{
+            updatedUser = await User.findByIdAndUpdate(userId, {fullName, bio}, {new:true}) // ✅
+        } else {
             const upload = await cloudinary.uploader.upload(profilePic);
-            updatedUser = await User.findByIdAndUpdate(userId, {profilePic:upload.secure_url, bio, fullName}, {new:true});
+            updatedUser = await User.findByIdAndUpdate(userId, {profilePic: upload.secure_url, bio, fullName}, {new:true});
         }
+
         res.json({success:true, user:updatedUser})
     } catch (error) {
         console.log(error.message)
